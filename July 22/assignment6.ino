230758b733ca0b54aa0278e5c7c9f30f6d333387
Austin Chang <31555305+l0rdf2l@users.noreply.github.com>
2020-07-22 01:03:45 -0700

    Added comments to Arduino Code

--- July 22/assignment6.ino
+++ July 22/assignment6.ino
@@ -1,34 +1,34 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
+  pinMode(8, OUTPUT);                                  // initialize digital pin 8 as an output
+  pinMode(13, OUTPUT);                                 // initialize digital pin 13 as an output
+  Serial.begin(9600);                                  // initialize serial communication at 9600 bits per second
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int switchPosition = digitalRead(A0);
-  int sensorValue = analogRead(A1);
+  int switchPosition = digitalRead(A0);                // read the switchPosition on analog pin 0
+  int sensorValue = analogRead(A1);                    // read the sensorValue on analog pin 1
 
-//  if (switchPosition == HIGH) {
-//    digitalWrite(8, HIGH);
-//    delay(1000);
-//    digitalWrite(13, LOW);
-//    delay(1000);
-//  } else  {
-//    digitalWrite(8, LOW);
-//    delay(1000);
-//    digitalWrite(13, HIGH);
-//    delay(1000);
-//  }
-
-  if (sensorValue > 900) {
-    digitalWrite(8, HIGH);
-    delay(1000);
-    digitalWrite(8, LOW);
-    delay(1000);
+  if (switchPosition == HIGH) {
+    digitalWrite(8, HIGH);                             // turn the LED on by making the voltage HIGH
+    delay(1000);                                       // wait for a second
+    digitalWrite(13, LOW);                             // turn the LED off by making the voltage LOW
+    delay(1000);                                       // wait for a second
+  } else  {
+    digitalWrite(8, LOW);                              // turn the LED off by making the voltage LOW
+    delay(1000);                                       // wait for a second
+    digitalWrite(13, HIGH);                            // turn the LED on by making the voltage HIGH
+    delay(1000);                                       // wait for a second
   }
 
-  Serial.println(sensorValue);
-  delay(1);
+  if (sensorValue > 900) {
+    digitalWrite(8, HIGH);                             // turn the LED on by making the voltage HIGH
+    delay(1000);                                       // wait for a second
+    digitalWrite(8, LOW);                              // turn the LED off by making the voltage LOW
+    delay(1000);                                       // wait for a second
+  }
+
+  Serial.println(sensorValue);                         // print out sensorValue
+  delay(1);                                            // slight delay between each sensorValue
 }
